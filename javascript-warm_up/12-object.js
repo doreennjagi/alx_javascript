3eb39a0911ec6b2a86ac2969dd7980c371564461
doreennjagi <jaydenrishi@gmail.com>
2023-09-06 00:49:59 +0300

    javscript-warm_up

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,9 +1,8 @@
-#!?usr/bin/node
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let i = 0; i < lines.length; i++) {
-if (lines[i].includes('12')) {
-    lines[i] = lines[i].replace('12', '89');
-}
-console.log(lines[i]);
-}
+#!/usr/bin/node
+const myObject = {
+    type: 'object',
+    value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
